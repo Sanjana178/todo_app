04dd2c2340bf8c437fdccf4d984b3bb32235dbc5
Sanjana <sanjana.anantbhat178@gmail.com>
2024-08-20 22:09:40 +0200

    4.1: generate task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_17_180621) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_20_200752) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "status"
+    t.datetime "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
