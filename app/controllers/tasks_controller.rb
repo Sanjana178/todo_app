dfb808af797340b6340581f0d03bf5f7c273589b
Sanjana <sanjana.anantbhat178@gmail.com>
2024-08-21 20:44:32 +0200

    5.2: implement read task functionality https://trello.com/c/lUiGXHQF/11-sanjana

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,26 @@
 class TasksController < ApplicationController
 
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
+    @task.user = current_user
 
     if @task.save
       redirect_to @task, notice: 'Task was successfully created.'
     else
       render :new
     end
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 
   private
